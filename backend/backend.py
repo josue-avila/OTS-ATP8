a93607fedd2db67f140a45e95b37fb9b125eca62
josue-avila <josue.avila@olist.com>
2021-01-04 16:56:53 -0300

    criar novo marketplace

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,5 @@
 
-def save_marketplces(name:str, description: str) -> None:
+def save_marketplaces(name:str, description: str) -> None:
     file = open('backend/db/marketplaces.txt','a')
     file.write(f'{name};{description}\n')
     file.close()
--- web/web.py
+++ web/web.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
 import sys
+sys.path.append('backend/')
 
-sys.path.append('backend')
-from backend import read_products
+from flask import Flask, render_template, request, redirect
+
+from backend import read_products,save_marketplaces
+
 
 app = Flask(__name__)
 
@@ -23,7 +25,18 @@ def product_list():
     return render_template('product_list.html', title = 'Produtos', list = products)
 
 @app.route('/marketplaces')
-def create_marketplace():
+def list_marketplace():
     return render_template('marketplaces.html', title = 'Marketplaces')
+
+@app.route('/new_marketplace')
+def new_marketplace():
+    return render_template('new_marketplace.html', title ='Novo Marketplace')
+
+@app.route('/create_marketplace',methods=['POST'])
+def create_markestplace():
+    name = request.form.get("name")
+    description = request.form.get("description")
+    save_marketplaces(name,description)
+    return redirect('/')
 
 app.run(debug=True)
