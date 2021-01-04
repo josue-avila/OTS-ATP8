86f1d88cd114f35b10eca0b37abce8ff646c94a3
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-04 16:46:45 -0300

    Listagem de produtos pelo txt

--- backend/backend.py
+++ backend/backend.py
@@ -3,3 +3,13 @@ def save_marketplces(name:str, description: str) -> None:
     file = open('backend/db/marketplaces.txt','a')
     file.write(f'{name};{description}\n')
     file.close()
+
+def read_products() -> list:
+    file = open('backend/db/products.txt', 'r')
+    products = []
+    for line in file:
+        line_by_comas = line.split(';')
+        products.append(line_by_comas)
+        products[-1][-1] = products[-1][-1].rstrip('\n')
+    file.close()
+    return products
--- web/web.py
+++ web/web.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import sys
+
+sys.path.append('backend')
+from backend import read_products
 
 app = Flask(__name__)
 
@@ -6,9 +10,17 @@ app = Flask(__name__)
 def menu():
     return render_template('menu.html')
 
-@app.route('/products')
+@app.route('/create-product')
 def create_product():
-    return render_template('product_list.html', title = 'Produtos')
+    products = read_products()
+    return render_template('create_product.html', title = 'Produtos', list = products)
+
+@app.route('/products')
+def product_list():
+    # se método post
+        # chamar função de criar produto
+    products = read_products()
+    return render_template('product_list.html', title = 'Produtos', list = products)
 
 @app.route('/marketplaces')
 def create_marketplace():
