86d818db05e8f0902c0165ecdaa729143874e8c5
josue-avila <josue.avila@olist.com>
2021-01-04 15:07:48 -0300

    criando backend

--- web/web.py
+++ web/web.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/products')
 def create_product():
     return 0
+
+@app.route('/marketplaces')
+def create_mktplace():
+    return render_template('marketplaces.html')
