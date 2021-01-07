61b2ea31acbded75d799f0f339af7f83dd797e7a
Regina Beretta <reginabmre@gmail.com>
2021-01-07 10:18:39 -0400

    eliminacao funcao redundante

--- backend/category.py
+++ backend/category.py
@@ -1,17 +1,10 @@
-from backend import create_log
+from backend import create_log, write_file
 cat_file = "backend/db/categories.txt"
-
-
-def salvar_arquivo(caminho:str,linha:str) -> None:
-    arquivo = open(caminho,'a')
-    arquivo.write(f'{linha}\n')
-    arquivo.close()
-
 
 def add_new_category(name:str, desc:str)->None:  
     if isinstance(name, str) and isinstance(desc, str): 
         linha = f'{name};{desc}'
-        salvar_arquivo(cat_file,linha)
+        write_file(cat_file,linha)
     create_log('create_category')    
 
 def read_categories() -> list:
