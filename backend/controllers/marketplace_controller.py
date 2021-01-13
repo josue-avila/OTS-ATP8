be6e29605274509cf2a112702a9dc7ea449e1003
Juliana Karoline <julianaklulo@gmail.com>
2021-01-13 17:51:54 -0300

    Adiciona funções para busca e atualização de marketplaces

--- backend/controllers/marketplace_controller.py
+++ backend/controllers/marketplace_controller.py
@@ -1,4 +1,4 @@
-from backend.dao.db.marketplace_dao import read_marketplaces_db, save_marketplace_db
+from backend.dao.db.marketplace_dao import read_marketplaces_db, save_marketplace_db, read_marketplace_db, update_marketplace_db
 from backend.controllers.log_controller import save_log
 from backend.models.marketplace import Marketplace
 
@@ -12,7 +12,19 @@ def save_marketplace(marketplace: Marketplace) -> None:
 
 def read_marketplaces() -> list:
     marketplaces = read_marketplaces_db()
-
     save_log('get', 'marketplaces')
-
     return marketplaces
+
+
+def read_marketplace(id: int) -> Marketplace:
+    marketplace = read_marketplace_db(id)
+    save_log('read', 'marketplace')
+    return marketplace
+
+
+def update_marketplace(marketplace: Marketplace) -> None:
+    new_marketplace = read_marketplace(marketplace.id)
+    new_marketplace.name = marketplace.name
+    new_marketplace.description = marketplace.description
+    update_marketplace_db(new_marketplace)
+    save_log('update', 'marketplace')
