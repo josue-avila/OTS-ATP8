bfb2b5031ec4e7f152d2fb8e1493a0f5112596c4
Juliana Karoline <julianaklulo@gmail.com>
2021-01-13 18:18:20 -0300

    Cria listagem e autalização de produto no db

--- backend/dao/db/product_dao.py
+++ backend/dao/db/product_dao.py
@@ -26,3 +26,14 @@ def read_products_db():
         product = Product(tuple[1], tuple[2], tuple[3], tuple[0])
         list_products.append(product)
     return list_products
+
+
+def read_product_db(id: int) -> Product:
+    cursor.execute(f"SELECT * FROM produto WHERE id = {id};")
+    tuple = cursor.fetchall()[0]
+    product = Product(tuple[1], tuple[2], tuple[3], tuple[0])
+    return product
+
+
+def update_product_db(product: Product) -> None:
+    cursor.execute(f"UPDATE produto SET nome = '{product.name}', descricao = '{product.description}', preco = '{product.price}' WHERE id = {product.id};")
