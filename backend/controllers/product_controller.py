fdb555d1b6945f2adab953c8cffc485fcd74d586
Juliana Karoline <julianaklulo@gmail.com>
2021-01-13 18:18:39 -0300

    Cria função de listagem e atualização de produto

--- backend/controllers/product_controller.py
+++ backend/controllers/product_controller.py
@@ -1,4 +1,4 @@
-from backend.dao.db.product_dao import save_product_db, read_products_db
+from backend.dao.db.product_dao import save_product_db, read_products_db, read_product_db, update_product_db
 from backend.controllers.log_controller import save_log
 from backend.models.product import Product
 
@@ -17,3 +17,18 @@ def save_product(product: Product) -> None:
             and isinstance(product.price, str)):
         save_product_db(product)
         save_log('set', 'product')
+
+
+def read_product(id: int) -> Product:
+    product = read_product_db(id)
+    save_log('read', 'product')
+    return product
+
+
+def update_product(product: Product) -> None:
+    new_product = read_product(product.id)
+    new_product.name = product.name
+    new_product.description = product.description
+    new_product.price = product.price
+    update_product_db(new_product)
+    save_log('update', 'product')
