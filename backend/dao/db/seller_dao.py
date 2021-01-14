dcf0c3b1c78077b9115f8e3ad9f2f9e98b4165d6
Juliana Karoline <julianaklulo@gmail.com>
2021-01-14 00:37:30 -0300

    Adiciona funções de buscar e editar seller no db

--- backend/dao/db/seller_dao.py
+++ backend/dao/db/seller_dao.py
@@ -3,7 +3,6 @@ from backend.models.seller import Seller
 
 
 def read_sellers_db() -> list:
-
     cursor.execute('SELECT * FROM seller ORDER BY id')
     sellers = cursor.fetchall()
     list_sellers = []
@@ -11,6 +10,7 @@ def read_sellers_db() -> list:
         seller = Seller(tuple[1], tuple[2], tuple[3], tuple[0])
         list_sellers.append(seller)
     return list_sellers
+
 
 def save_seller_db(seller: Seller) -> None:
     try:
@@ -20,3 +20,14 @@ def save_seller_db(seller: Seller) -> None:
         return True
     except Exception as e:
         return False
+
+
+def read_seller_db(id: int) -> Seller:
+    cursor.execute(f"SELECT * FROM seller WHERE id = {id};")
+    tuple = cursor.fetchall()[0]
+    seller = Seller(tuple[1], tuple[2], tuple[3], tuple[0])
+    return seller
+
+
+def update_seller_db(seller: Seller) -> None:
+    cursor.execute(f"UPDATE seller SET nome = '{seller.fullname}', telefone = '{seller.phone}', email = '{seller.email}' WHERE id = {seller.id};")
