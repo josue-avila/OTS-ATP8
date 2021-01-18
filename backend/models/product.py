9333f0fab808bfbc69f770c888c8c6bc8faebe50
Juliana Karoline <julianaklulo@gmail.com>
2021-01-18 13:39:54 -0300

    Refatora model de produto para usar properties

--- backend/models/product.py
+++ backend/models/product.py
@@ -1,11 +1,39 @@
 class Product:
-    name: str
-    description: str
-    price: float
-    id: int
+    __name: str
+    __description: str
+    __price: float
+    __id: int
 
     def __init__(self, name: str, description: str, price: float, id: int = None):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.id = id
+        self.__name = name
+        self.__description = description
+        self.__price = price
+        self.__id = id
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def description(self) -> str:
+        return self.__description
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @property
+    def id(self) -> int:
+        return self.__id
+
+    @name.setter
+    def name(self, name: str) -> None:
+        self.__name = name
+
+    @description.setter
+    def description(self, description: str) -> None:
+        self.__description = description
+
+    @price.setter
+    def price(self, price: float) -> None:
+        self.__price = price
