f16c0bae0d6ee49066f27a6be2fb03cc97235db1
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-25 13:30:37 -0300

    Alteração em classe controller seller

--- backend/controllers/seller_controller.py
+++ backend/controllers/seller_controller.py
@@ -1,7 +1,6 @@
 from backend.controllers.base_controller import BaseController
 from backend.dao.db.seller_dao import SellerDao
 
-
 class SellerController(BaseController):
     def __init__(self):
         self.__dao = SellerDao()
