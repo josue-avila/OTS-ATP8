f790e2da8018bf1f7830109b1d8b1b6044836e31
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-25 13:30:01 -0300

    Alterando classe seller para padrão orm

--- backend/models/seller.py
+++ backend/models/seller.py
@@ -1,18 +1,20 @@
-class Seller:
-    __fullname: str
-    __phone: str
-    __email: str
-    __id: int
+from sqlalchemy import Column, String
+from backend.models.base_model import BaseModel
 
-    def __init__(self, fullname: str, phone: str, email: str, id: int = None):
-        self.__fullname = fullname
-        self.__phone = phone
-        self.__email = email
-        self.__id = id
+class Seller(BaseModel):
+    __tablename__ = 'sellers'
+    __name = Column('fullname', String(length=100) )
+    __phone = Column( String(length=20) )
+    __email = Column( String(length=100) )
+
+    def __init__(self, name: str, phone: str, email: str, id: int = None):
+        self.name = name
+        self.phone = phone
+        self.email = email
 
     @property
-    def fullname(self) -> str:
-        return self.__fullname
+    def name(self) -> str:
+        return self.__name
     
     @property
     def phone(self) -> str:
@@ -22,13 +24,9 @@ class Seller:
     def email(self) -> str:
         return self.__email
 
-    @property
-    def id(self) -> int:
-        return self.__id
-
-    @fullname.setter
-    def fullname(self, fullname: str) -> None:
-        self.__fullname = fullname
+    @name.setter
+    def name(self, name: str) -> None:
+        self.__name = name
 
     @phone.setter
     def phone(self, phone: str) -> None:
@@ -37,7 +35,3 @@ class Seller:
     @email.setter
     def email(self, email: str) -> None:
         self.__email = email
-    
-    @id.setter
-    def id(self, id: int) -> None:
-        self.__id = id
