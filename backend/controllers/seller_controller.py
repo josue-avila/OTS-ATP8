f3b7670b656c95d5ae0d9698cbe5ad8840beca64
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-25 13:49:49 -0300

    Adicionando linha

--- backend/controllers/seller_controller.py
+++ backend/controllers/seller_controller.py
@@ -1,6 +1,7 @@
 from backend.controllers.base_controller import BaseController
 from backend.dao.db.seller_dao import SellerDao
 
+
 class SellerController(BaseController):
     def __init__(self):
         self.__dao = SellerDao()
