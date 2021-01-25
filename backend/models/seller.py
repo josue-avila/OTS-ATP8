955dfafa1ebf86dbee0a8205716865b97475586f
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-25 14:16:14 -0300

    Alteranado atributos para publico

--- backend/models/seller.py
+++ backend/models/seller.py
@@ -3,35 +3,11 @@ from backend.models.base_model import BaseModel
 
 class Seller(BaseModel):
     __tablename__ = 'seller'
-    __name = Column('name', String(length=100) )
-    __phone = Column('phone', String(length=18) )
-    __email = Column('email', String(length=30) )
+    name = Column('name', String(length=100) )
+    phone = Column('phone', String(length=18) )
+    email = Column('email', String(length=30) )
 
     def __init__(self, name: str, phone: str, email: str, id: int = None):
         self.name = name
         self.phone = phone
         self.email = email
-
-    @property
-    def name(self) -> str:
-        return self.__name
-    
-    @property
-    def phone(self) -> str:
-        return self.__phone
-
-    @property
-    def email(self) -> str:
-        return self.__email
-
-    @name.setter
-    def name(self, name: str) -> None:
-        self.__name = name
-
-    @phone.setter
-    def phone(self, phone: str) -> None:
-        self.__phone = phone
-    
-    @email.setter
-    def email(self, email: str) -> None:
-        self.__email = email
