4b5cfad35a2bcd85b98bbecf5ddd2b850ca51c01
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-25 14:30:57 -0300

    Testes atualizados

--- tests/seller/controller.py
+++ tests/seller/controller.py
@@ -5,14 +5,12 @@ from backend.models.seller import Seller
 seller_controller = SellerController()
 seller = Seller('clara', '(53) 0 4254-4324', 'clara@olist.com')
 
-def test_create_seller_should_return_none():
+def test_method_seller_should_return_none():
     assert seller_controller.create(seller) == None
+    assert seller_controller.delete(1) == None
 
 def test_read_sellers_should_return_list():
     assert isinstance(seller_controller.read_all(), list)
-
-def test_delete_seller_should_return_none():
-    assert seller_controller.delete(1) == None
 
 def test_compare_instance_seller_controller_to_basecontroller():
     assert isinstance(seller_controller, BaseController)
--- tests/seller/models.py
+++ tests/seller/models.py
@@ -9,23 +9,14 @@ phone = '(41) 9 3542-3456'
 identifier = 942
 seller = Seller(name, phone, email, identifier)
 
-def test_compare_name_model_to_var():
+def test_compare_column_model_to_var():
     assert seller.name == name
-
-def test_compare_email_model_to_var():
     assert seller.email == email
-
-def test_compare_phone_model_to_var():
     assert seller.phone == phone
-
-def test_compare_id_model_to_var():
     assert seller.id_ == identifier
 
-def test_compare_type_name_model_to_str():
+def test_compare_type_column_model_to_str():
     assert type(seller.name) == str
-
-def test_compare_type_email_model_to_str():
     assert type(seller.email) == str
-
-def test_compare_type_id_model_to_int():
+    assert type(seller.phone) == str
     assert type(seller.id_) == int
