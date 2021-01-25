21d382c465ca51d3d1cd2b2739ba4c5a70517b5c
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-25 13:44:18 -0300

    Category controller refactored

--- backend/controllers/base_controller.py
+++ backend/controllers/base_controller.py
@@ -13,27 +13,27 @@ class BaseController:
     def create(self, model: BaseModel) -> None:
         self.__dao.save(model)
         log = Log('set', self.__domain_name)
-        self.__log_controller.create(log)
+        # self.__log_controller.create(log)
 
     def read_by_id(self, id: int) -> BaseModel:
         result = self.__dao.read_by_id(id)
         log = Log('get', self.__domain_name)
-        self.__log_controller.create(log)
+        # self.__log_controller.create(log)
         return result
 
     def read_all(self) -> list:
         result = self.__dao.read_all()
         log = Log('get', f'all {self.__domain_name}')
-        self.__log_controller.create(log)
+        # self.__log_controller.create(log)
         return result
 
     def delete(self, id: int) -> None:
         item = self.read_by_id(id)
         self.__dao.delete(item)
         log = Log('delete', self.__domain_name)
-        self.__log_controller.create(log)
+        # self.__log_controller.create(log)
 
     def update(self, model: BaseModel) -> None:
         self.__dao.save(model)
         log = Log('update', self.__domain_name)
-        self.__log_controller.create(log)
+        # self.__log_controller.create(log)
