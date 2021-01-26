9e4e1637358cc7f2a52fadb3d630c196a1a510a9
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-26 18:41:48 -0300

    Added validation to 'email' and 'phone' attributes

--- backend/models/seller.py
+++ backend/models/seller.py
@@ -1,11 +1,28 @@
+import re
+
 from sqlalchemy import Column, String
+from sqlalchemy.orm import validates
+
 from backend.models.base_model import BaseModel
+
 
 class Seller(BaseModel):
     __tablename__ = 'seller'
     name = Column('name', String(length=100) )
     phone = Column('phone', String(length=18) )
     email = Column('email', String(length=30) )
+
+    @validates('email')
+    def validate_email(self, key, email):
+        if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return email
+        raise ValueError('Invalid email!')
+
+    @validates('phone')
+    def validate_phone(self, key, phone):
+        if not re.search('[a-zA-Z]', phone):
+            return phone
+        raise ValueError("Phone can't have a character")
 
     def __init__(self, name: str, phone: str, email: str):
         self.name = name
