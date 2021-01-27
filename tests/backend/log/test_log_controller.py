ab944d754bbdd461a1f84fceb30a4785d36e87ca
anapaula-nestor <anapaula.nestor@olist.com>
2021-01-27 11:13:42 -0300

    log_improvement

--- tests/backend/log/test_log_controller.py
+++ tests/backend/log/test_log_controller.py
@@ -1,6 +1,20 @@
 from backend.controllers.log_controller import LogController
+from backend.models.log import Log
+
+log = Log('teste', 'test')
+log_controller = LogController()
 
 
 def test_log_controller_instance():
-    log_controller = LogController()
     assert isinstance(log_controller, LogController)
+
+
+def test_method_save_log_should_return_primary_key():
+    result = log_controller.save(log)
+    assert isinstance(result, int)
+
+
+def tes_method_readall_must_return_list():
+    assert isinstance(log_controller.read_all() ,list)
+
+
--- tests/backend/log/test_log_model.py
+++ tests/backend/log/test_log_model.py
@@ -2,7 +2,7 @@ from backend.models.base_model import BaseModel
 from backend.models.log import Log
 
 
-def test_product_model_instance():
+def test_log_model_instance():
     operetion = 'set'
     description = 'Descricao'
 
@@ -12,3 +12,18 @@ def test_product_model_instance():
     assert isinstance(log_model, BaseModel)
     assert log_model.operation == operetion
     assert log_model.description == description
+
+
+def test_validate_operetion():
+    try:
+        Log('', 'test')
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
+
+def test_validate_description():
+    try:
+        Log('Test', None)
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
