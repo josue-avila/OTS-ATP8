95a3e6d9ce500ed92f32bfd7dff516db3373359a
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-27 10:11:34 -0300

    Added new validations to phone, email and name

--- backend/models/seller.py
+++ backend/models/seller.py
@@ -8,21 +8,39 @@ from backend.models.base_model import BaseModel
 
 class Seller(BaseModel):
     __tablename__ = 'seller'
-    name = Column('name', String(length=100) )
-    phone = Column('phone', String(length=18) )
-    email = Column('email', String(length=30) )
+    name = Column('name', String(length=100))
+    phone = Column('phone', String(length=18))
+    email = Column('email', String(length=30))
 
     @validates('email')
     def validate_email(self, key, email):
-        if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        if email is None:
+            raise ValueError("Please write a email number!")
+        elif email.strip(' ') == '':
+            raise ValueError("Email can't be null!")
+        elif re.match(r"[^@]+@[^@]+\.[^@]+", email):
             return email
         raise ValueError('Invalid email!')
 
     @validates('phone')
     def validate_phone(self, key, phone):
-        if not re.search('[a-zA-Z]', phone):
-            return phone
-        raise ValueError("Phone can't have a character")
+        if phone is None:
+            raise ValueError("Please write a phone number!")
+        elif phone.strip(' ') == '':
+            raise ValueError("Phone can't be null!")
+        elif re.search('[a-zA-Z]', phone):
+            raise ValueError("Phone can't have a character!")
+        return phone
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if name is None:
+            raise ValueError("Please write a name number!")
+        elif name.strip(' ') == '':
+            raise ValueError("Name can't be null!")
+        elif name.strip(' ') != '':
+            return name
+        raise ValueError("Name can't be null!")
 
     def __init__(self, name: str, phone: str, email: str):
         self.name = name
